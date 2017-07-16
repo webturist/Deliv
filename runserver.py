5c48dd3f8246356d0df43679f02702f42ed98797
webturist <webturist@gmail.com>
2017-07-17 00:59:53 +0300

    переход на VS

--- runserver.py
+++ runserver.py
@@ -1,4 +1,11 @@
+from os import environ
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+    
